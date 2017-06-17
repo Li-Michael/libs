39d911812dc5f3b84a42cf533f7a91ce3f62f72f
Michael <Lizz007sci@126.com>
2017-06-17 20:42:10 +0800

    add num_formats.py

--- reloadall.py
+++ reloadall.py
@@ -17,7 +17,16 @@ def transitive_reload(module, visited):
         reload(module)                              # And visit children
         visited[module] = None
         for attrobj in module.__dict__.values():    # For all attrs Recur if module
-            if type(attrobj) == types.ModuleType:  # if isinstance(attrobj, types.ModuleType)
+            if type(attrobj) == types.ModuleType:   # if isinstance(attrobj, types.ModuleType)
                 transitive_reload(attrobj, visited)
 
+def reload_all(*args):
+    visited = {}
+    for arg in args:
+        if type(arg) == types.ModuleType:
+            transitive_reload(arg, visited)
+
+if __name__='__main__':                             # Test code:reload myself, should reload this, types
+    import reloadall
+    reload_all(reloadall)
 
